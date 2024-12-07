ed062ba02f3f2890f62cdfa59493ab985790db4d
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-12-07 15:01:12 -0300

    Finalizado Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,19 @@
+import logo from "../assets/exemplo1.png"
+import { FaLinkedin } from "react-icons/fa"
+import { FaGithub } from "react-icons/fa"
+import { SiWakatime } from "react-icons/si";
+
 const Navbar = () => {
-    return <div>Navbar</div>
+    return  <nav className="mb-20 flex items-center justify-between py-6">
+        <div className="flex flex-shrink-0 items-center">
+            <img className="mx-2 w-10" src= {logo} alt="logo" />
+        </div>
+        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+            <FaLinkedin />
+            <FaGithub />
+            <SiWakatime />
+        </div>
+    </nav>
 }
 
 export default Navbar

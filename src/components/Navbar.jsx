e32be7bd609c70d1f012ea9af2f7bdb1619ceef4
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-12-08 14:58:48 -0300

    Lincado dados do NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-import logo from "../assets/exemplo1.png"
 import { FaLinkedin } from "react-icons/fa"
 import { FaGithub } from "react-icons/fa"
 import { SiWakatime } from "react-icons/si";
@@ -6,12 +5,12 @@ import { SiWakatime } from "react-icons/si";
 const Navbar = () => {
     return  <nav className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-            <img className="mx-2 w-10" src= {logo} alt="logo" />
+            <h1 className="mx-2 w-10 text-5xl">JV</h1>
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-            <FaLinkedin />
-            <FaGithub />
-            <SiWakatime />
+            <a href="https://www.linkedin.com/in/joao-victorcm-dev/" target="_blank"><FaLinkedin/></a>
+            <a href="https://github.com/Joao-VictorCm" target="_blank"><FaGithub /></a>
+            <a href="https://wakatime.com/@JoaoVictorCm" target="_blank"><SiWakatime /></a>
         </div>
     </nav>
 }

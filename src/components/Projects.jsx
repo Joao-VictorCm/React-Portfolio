e2f5e8de22222c8f15098ae71b445c85fb59cea0
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-12-07 21:00:28 -0300

    Finalizado seção projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,7 +12,17 @@ const Projects = () => {
                    <img src={projects.image} 
                    width={150}
                    height={150}
-                   alt={projects.title}/>
+                   alt={projects.title}
+                   className="mb-6 rounded"/>
+                </div>
+                <div className="w-full max-w-xl lg:w-3/4">
+                   <h6 className="mb-2 font-semibold">{projects.title}</h6>
+                   <p className="mb-4 text-neutral-400">{projects.description}</p>
+                   {projects.technologies.map((tech, map) => 
+                    <span key={index} className="mr-2 rouded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900 ">
+                        {tech}
+                    </span>
+                   )}
                 </div>
               </div>
             )}
